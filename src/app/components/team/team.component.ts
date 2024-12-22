acde7281fef49ea107b4fad9acc231dcc00cd703
Marcelo Junior <Marcelo.Junior@fit-tecnologia.org.br>
2024-12-22 17:23:25 -0400

    Add new components at team

--- src/app/components/team/team.component.ts
+++ src/app/components/team/team.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
-  styleUrls: ['./team.component.scss']
+  styleUrls: ['./team.component.scss'],
 })
 export class TeamComponent {
-
+  teamMembers = [
+    {
+      name: 'Cezar Duque',
+      role: 'Mestre de Obras',
+      description:
+        'Especialista em BIM, possui experiência em projetos residenciais e industriais.',
+      photo: 'assets/images/Cezar.png',
+    },
+    {
+      name: 'Charlie Duque',
+      role: 'Eletricista',
+      description:
+        'Com experiência em construções e especialista em levantamento e análise topográfica.',
+      photo: 'assets/images/Charlie.png',
+    },
+  ];
 }
