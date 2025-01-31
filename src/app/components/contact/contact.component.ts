f7a94b460c926a35588477d6f79bed5e000b77e3
Marcelo Junior <Marcelo.Junior@fit-tecnologia.org.br>
2025-01-30 22:35:19 -0400

    one stap back

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ContactService } from '../services/contact.service'; // Correct import path
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -15,7 +17,8 @@ export class ContactComponent {
   constructor(
     private fb: FormBuilder,
     private contactService: ContactService, // Inject ContactService here!
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private http: HttpClient
   ) {
     this.contactForm = this.fb.group({
       name: ['', Validators.required],
@@ -35,7 +38,7 @@ export class ContactComponent {
 
     this.isLoading = true;
 
-    this.contactService.enviarEmail(this.contactForm.value).subscribe( // Use the injected service
+    this.http.post('https://cezar-backend.vercel.app/api/send-email', this.contactForm.value).subscribe( // Use the injected service
       (response) => {
         this.snackBar.open('Formulário enviado com sucesso!', 'Fechar', {
           duration: 3000,
